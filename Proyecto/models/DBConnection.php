59b9b894e0c97b59320f4803afbd436fdc7d6d06
franc97 <fpinasilva@gmail.com>
2022-10-30 19:38:04 -0600

    Update DBConnection.php

--- Proyecto/models/DBConnection.php
+++ Proyecto/models/DBConnection.php
@@ -6,7 +6,7 @@
         public static function getConnection(){
             if(self::$connection == null){
                 //Crear objeto de connexión
-                self::$connection = new PDO('mysql:host=localhost;dbname=fdfpotcast;charset=utf8', 'root', 'Velozirraptor9*');
+                self::$connection = new PDO('mysql:host=localhost;dbname=fdfpotcast;charset=utf8', 'root', '');
                 self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             }
